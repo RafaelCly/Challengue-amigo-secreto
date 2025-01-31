59ca7d45b54254a6b4b3dcaedfd028ef1712f5eb
RafaelCly <rafael.chu.l@uni.pe>
2025-01-30 19:16:51 -0500

    Elminar la funcion ConIn porque es innecesaria

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
-
-function Cond_In() {
-    amigos = [];
-}
-
 function asignarTexto (element,text) {
     let a = document.getElementById(element).value;
     a.innerHTML = text;
